432996fdabb9f4bbff3b99b205d12afc9a2cbb45
小坂和也 <shinozu1316@icloud.com>
2021-04-29 20:20:05 +0900

    first-commit

---
+++ resources/views/hello/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>COACHTECH</title>
+  <style>
+    p {
+      font-size: "16px";
+    }
+  </style>
+</head>
+
+<body>
+  <h1>タイトル</h1>
+  <p>{{$content}}</p>
+</body>
+
+</html>
